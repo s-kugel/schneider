5d92ad2c31d2edd83f93ddcf436b9836689b4a5d
i-zacky <aizaki.takayuki@gmail.com>
2024-07-10 03:07:23 +0900

    setup docker database & flyway

--- schneider-eule-db/build.gradle.kts
+++ schneider-eule-db/build.gradle.kts
@@ -1,3 +1,34 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.flyway.mysql)
+    }
+}
+
 plugins {
     `java-library`
+    idea
+    eclipse
+    alias(libs.plugins.flyway)
+    alias(libs.plugins.spotless)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.mysql)
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+flyway {
+    url = "jdbc:mysql://${System.getenv("EULE_DB_HOST")}:${System.getenv("EULE_DB_PORT")}/${System.getenv("EULE_DB_NAME")}"
+    user = System.getenv("EULE_DB_USER")
+    password = System.getenv("EULE_DB_PASSWORD")
 }
