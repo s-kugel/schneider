bf7ccb5ed0dacc7bcd6d1b502dc671ae5870cb84
i-zacky <aizaki.takayuki@gmail.com>
2024-07-15 22:11:07 +0900

    schneider-backoffice-apiを別リポジトリへ分離

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,9 @@
 rootProject.name = "schneider"
 
-include("backoffice-api")
 include("batch")
 include("common")
 include("front-api")
 
-project(":backoffice-api").projectDir = File("schneider-backoffice-api")
 project(":batch").projectDir = File("schneider-batch")
 project(":common").projectDir = File("schneider-common")
 project(":front-api").projectDir = File("schneider-front-api")
